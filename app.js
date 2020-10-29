738c4019941b992737697e25905e7827674a9b0e
s3prototype <liuqahs15@gmail.com>
2020-10-29 13:50:25 -0400

    Anothern

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.send("Welcome to the home page");
-});
-
 const port = process.env.port || 8080;
 
 app.listen(port, () => {
     console.log("Whatup");
 });
 
+
+app.get('/', (req, res) =>{
+    res.send("Welcome to the home page");
+    res.send("Port is" + port);
+});
